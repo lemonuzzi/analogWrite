70f06fcaf9150da0dab71bf6e50a5f50b01b44ae
lemonuzzi <leo.lamanuzzi@gmail.com>
2019-02-09 14:05:23 -0500

    Initial commit

---
+++ analogWrite.ino
@@ -0,0 +1,128 @@
+#define PWM_MAX_DUTY      255
+#define PWM_MIN_DUTY      50
+#define PWM_START_DUTY    100
+
+int bldc_step = 0;
+
+//Outputs
+int en1 = 3;   //EN1
+int en2 = 4;   //EN2
+int en3 = 5;   //EN3
+int in1 = 9;   //IN1
+int in2 = 10;  //IN2  
+int in3 = 11;  //IN3
+
+//Virtual neutral point
+int vnn = 6; 
+
+//Inputs
+int dig7 = 7;    //ADC1
+int analog2 = 2;  //ADC2
+int analog3 = 3;  //ADC3
+
+//duty values
+int duty = PWM_START_DUTY; 
+
+void setup() {
+  // enable
+  pinMode(en1, OUTPUT);
+  pinMode(en2, OUTPUT);
+  pinMode(en3, OUTPUT);
+
+  // PWM pins 
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+
+  // inputs from motor using on-chip ADC 
+  pinMode(analog2, INPUT);
+  pinMode(analog3, INPUT);
+  pinMode(dig7, INPUT);
+
+  // virtual neutral point for comparator circuit
+  pinMode(vnn, OUTPUT);
+}
+
+// BLDC motor commutation function
+void bldc_move(){        
+  switch(bldc_step){
+    case 0:
+      AH_BL();
+      break;
+    case 1:
+      AH_CL();
+      break;
+    case 2:
+      BH_CL();
+      break;
+    case 3:
+      BH_AL();
+      break;
+    case 4:
+      CH_AL();
+      break;
+    case 5:
+      CH_BL();
+      break;
+  }
+}
+
+void loop() {
+  bldc_move();
+  Serial.println("Case :" + bldc_step);
+  bldc_step++;
+  bldc_step %= 6;
+}
+
+// Vary duty cycle based on closed-loop parameters
+void varyDuty(){ 
+}
+
+// 6-STEP CASES
+void AH_CL(){
+  analogWrite(en1, HIGH);
+  analogWrite(en2, HIGH);
+  analogWrite(en3, LOW);
+  analogWrite(in1, HIGH);
+  analogWrite(in2, LOW);
+}
+
+void BH_CL(){
+  analogWrite(en1, LOW);
+  analogWrite(en2, HIGH);
+  analogWrite(en3, HIGH);
+  analogWrite(in2, HIGH);
+  analogWrite(in3, LOW);
+}
+
+void BH_AL(){
+  analogWrite(en1, HIGH);
+  analogWrite(en2, HIGH);
+  analogWrite(en3, LOW);
+  analogWrite(in1, LOW);
+  analogWrite(in2, HIGH);
+}
+
+void CH_AL(){
+  analogWrite(en1, HIGH);
+  analogWrite(en2, LOW);
+  analogWrite(en3, HIGH);
+  analogWrite(in1, LOW);
+  analogWrite(in3, HIGH);
+}
+
+void CH_BL(){
+  analogWrite(en1, LOW);
+  analogWrite(en2, HIGH);
+  analogWrite(en3, HIGH);
+  analogWrite(in2, LOW);
+  analogWrite(in3, HIGH);
+}
+
+void AH_BL(){
+  analogWrite(en1, HIGH);
+  analogWrite(en2, HIGH);
+  analogWrite(en3, LOW);
+  analogWrite(in1, HIGH);
+  analogWrite(in3, LOW);
+}
