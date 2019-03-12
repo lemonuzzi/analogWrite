a3c715c615244853bbdbb1b9df135c454f705bf0
Fred-Éric Désulmé <fred-eric.desulme@mail.mcgill.ca>
2019-03-12 13:23:55 -0400

    Update analogWrite.ino
    Add prepositioning code in motor start up sequence

--- analogWrite.ino
+++ analogWrite.ino
@@ -2,13 +2,15 @@
 
 #define PWM_MAX_DUTY      255
 #define PWM_MIN_DUTY      50
-#define PWM_START_DUTY    255
+#define PWM_START_DUTY    0
 
 //testing commit
 
 int bldc_step = 0;
 int i = 7000;
 int sensorValue;
+int i_prep = 0;
+
 
 //duty values
 int duty = PWM_START_DUTY;
@@ -16,11 +18,11 @@ int duty = PWM_START_DUTY;
 void setup() {
   Serial.begin(9600);
   
-  //Enable pins
+  // Enable pins
   DDRD = B00011100;    // Configure pins 2, 3 and 4 as outputs (Enables)
   PORTD = B00000000;   // (EN1=Pin 2, EN2=Pin 3, EN3=Pin 4)
 
-  //Input pins
+  // Input pins
   DDRB = B00001110;    // Configure pins 9, 10 and 11 as outputs (Inputs)
   PORTB = B00000000;   // (IN1=INA=Pin 9, IN2=INB=Pin 10, IN3=INB=Pin 11)
 
@@ -30,13 +32,9 @@ void setup() {
   // Timer2 module setting: set clock source to clkI/O / 1 (no prescaling)
   TCCR2A = 0;
   TCCR2B = 0x01;
-
-  //Set PWM Duty cycle
-  OCR1A = duty; //Pin 9
-  OCR1B = duty; //Pin 10
-  OCR2A = duty; //Pin 11
-
-  ACSR   = 0x10;           // Disable and clear (flag bit) analog comparator interrupt
+  
+  // Disable and clear (flag bit) analog comparator interrupt
+  ACSR   = 0x10;           
 }
 
 ISR (ANALOG_COMP_vect) {
@@ -87,7 +85,7 @@ void bldc_move() {
 }
 
 void loop() {
-  // Motor start & Run
+  // Motor start & Run (For testing)
 //  while (i >= 200) {
 //    delayMicroseconds(i);
 //    bldc_move();
@@ -99,6 +97,17 @@ void loop() {
 //    Serial.println(i);
 //  }
 
+  // Prespositioning Section
+  setDuty();
+  while(i_prep < 17){
+    AH_BL_CL();
+    duty = duty + 15
+    i++;
+    delay(10);
+    setDuty();
+  }
+  
+  // Ramp up sequence
   while (i >= 200) {
     delayMicroseconds(i);
     bldc_move();
@@ -107,14 +116,20 @@ void loop() {
     i = i - 20;
     Serial.println(i);
   }
-  ACSR |= 0x08;                    // Enable analog comparator interrupt
+  
+  // Enable analog comparator interrupt
+  ACSR |= 0x08;                    
   while (1) {
 
   }
 }
 
 // Vary duty cycle based on closed-loop parameters
-void varyDuty() {
+void setDuty() {
+  //Set PWM Duty cycle
+  OCR1A = duty; //Pin 9
+  OCR1B = duty; //Pin 10
+  OCR2A = duty; //Pin 11
 }
 
 void BEMF_A_RISING() {
